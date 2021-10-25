4ce6bd5046f8965464ed4db7e9a785a291b19413
Vanama <S543064@nwmissouri.edu>
2021-10-25 11:49:14 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
-var express = require('My Data');
+var express = require('MyData');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('My Data', { title: 'Mohith Naga Venkata Sai Vanama' });
+  res.render('MyData', { title: 'Mohith Naga Venkata Sai Vanama' });
 });
 
 module.exports = router;

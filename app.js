950db45b658e080e88821b47c7511650317fa392
vanamamohith <saimohith.97@gmail.com>
2021-10-27 20:52:02 -0500

    modified app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
 

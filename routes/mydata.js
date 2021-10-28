2971784020c5dea10b634890085326122638b2b2
vanamamohith <saimohith.97@gmail.com>
2021-10-27 20:47:54 -0500

    modified mydata

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
-var express = require('MyData');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('MyData', { title: 'Mohith Naga Venkata Sai Vanama' });
+  res.render('mydata', { title: 'Mohith Naga Venkata Sai Vanama' });
 });
 
 module.exports = router;
